32675e191a282627b8bcc6cda6b13ca351f7dfe3
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2020-01-26 05:24:05 +0800

    Re-commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// This file was created using:
+// npx tailwind init
+// this would work is tailwind is installed using npm but unfortunately I used yarn
+// through yarn: yarn tailwind init app/javascript/stylesheets/tailwind.config.js
+// head over to app/javascript/stylesheets/tailwind.config.js instead
+module.exports = {
+  theme: {
+    extend: {}
+  },
+  variants: {
+  	backgroundColor: ['responsive', 'hover', 'focus', 'active']
+  },
+  plugins: []
+}
